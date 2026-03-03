82be537cf0e77a40f4eb693347a299f0aab50e7a
Nick Marigo <118312264+Nick-Marigo@users.noreply.github.com>
2026-03-03 14:41:41 -0800

    Started to build game and understand how to implement click and drag mechanics

--- src/scenes/postcardGame.js
+++ src/scenes/postcardGame.js
@@ -2,4 +2,28 @@ class PostcardGame extends Phaser.Scene {
     constructor() {
         super("postcardGameScene")
     }
+
+    preload() {
+        this.load.path = "./assets/"
+        this.load.image("square", "Square.png")
+    }
+
+    create() {
+
+        this.square = this.add.sprite(500, 350, "square").setInteractive();
+        this.square2 = this.add.sprite(500, 350, "square").setTint(0xFF0000).setInteractive();
+
+        this.input.setDraggable(this.square);
+        this.input.setDraggable(this.square2);
+
+        this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+            gameObject.x = dragX;
+            gameObject.y = dragY;
+        })
+
+    }
+
+    update() {
+
+    }
 }
