1d186b5860e79504c0576fc55d026231b10e7dcc
Nick Marigo <nmarigo@ucsc.edu>
2026-03-08 16:02:35 -0700

    Finished the mechanics of air Filter state. Still need to add visual queues

--- src/prefabs/generator.js
+++ src/prefabs/generator.js
@@ -52,11 +52,20 @@ class Generator extends Phaser.GameObjects.Sprite {
         this.generatorImage = this.scene.add.sprite(width / 2, 50, 'generator', 0).setInteractive().setOrigin(.5, 0);
         this.generatorCover = this.scene.add.sprite(width / 2, 50, 'generatorCover').setInteractive().setOrigin(.5, 0).setDepth(100);
         this.scene.input.setDraggable(this.generatorCover);
-        this.targetGeneratorCover = this.scene.add.rectangle(150, 650, 540, 360, 0xffff00, 0.25);
+        this.targetGeneratorCover = this.scene.add.rectangle(200, 675, 540, 360, 0xffff00, 0.25);
 
         //create all images for airfilter state
+
+        //Air filter dirty and clean
         this.airFilterDirty = this.scene.add.sprite(this.generatorImage.x -161, this.generatorImage.y + 105, 'airFilter', 1).setInteractive().setOrigin(0.5, 0);
-        this.airFilerCover = this.scene.add.sprite(this.generatorImage.x - 159, this.generatorImage.y + 91, 'airFilterCover').setInteractive().setOrigin(0.5, 0);
+        this.targetairFilterDirty = this.scene.add.rectangle(550, 550, 60, 40, 0xffff000, 0.25).setVisible(false);
+        this.airFilterClean = this.scene.add.sprite(550, 490, 'airFilter', 0).setInteractive().setOrigin(0.5, 0);
+        this.targetairFilterClean = this.scene.add.rectangle(this.generatorImage.x - 161, this.generatorImage.y + 125, 60, 40, 0xffff000, 0.25).setVisible(false);
+
+        //Air filter cover
+        this.airFilterCover = this.scene.add.sprite(this.generatorImage.x - 159, this.generatorImage.y + 91, 'airFilterCover').setInteractive().setOrigin(0.5, 0);
+        this.targetairFilterCoverOFF = this.scene.add.rectangle(550, 625, 70, 80, 0xffff000, 0.25).setVisible(false);
+        this.targetairFilterCoverON = this.scene.add.rectangle(this.generatorImage.x - 159, this.generatorImage.y + 91, 70, 80, 0xffff000, 0.25).setOrigin(0.5, 0).setVisible(false);
 
     }
 
@@ -85,6 +94,8 @@ class BrokenState extends State {
                 generator.generatorCover.disableInteractive();
                 console.log("Cover snapped into place");
                 generator.brokenStep++;
+                generator.targetGeneratorCover.setVisible(false);
+                generator.targetairFilterCoverOFF.setVisible(true);
             }
         }
 
@@ -95,7 +106,8 @@ class AirFilterState extends State {
     enter(scene, generator){
         console.log("Enter airFilterState");
         scene.input.setDraggable(generator.airFilterDirty);
-        scene.input.setDraggable(generator.airFilerCover);
+        scene.input.setDraggable(generator.airFilterCover);
+        scene.input.setDraggable(generator.airFilterClean);
         
         //generator.square3.setInteractive();
 
@@ -103,11 +115,74 @@ class AirFilterState extends State {
 
     }
 
-    handleDrop(gameObject) {
+    handleDrop(gameObject, generator) {
+
+        if(gameObject === generator.airFilterCover) {
+            if(Phaser.Geom.Intersects.RectangleToRectangle(generator.airFilterCover.getBounds(), generator.targetairFilterCoverOFF.getBounds())) {
+                generator.airFilterCover.x = generator.targetairFilterCoverOFF.x
+                generator.airFilterCover.y = generator.targetairFilterCoverOFF.y - 25
+                generator.airFilterCover.disableInteractive();
+                console.log("Air Filter Cover snapped into place");
+                generator.airFilterStep++;
+                generator.targetairFilterCoverOFF.setVisible(false);
+                generator.targetairFilterDirty.setVisible(true);
+            }
+            if(Phaser.Geom.Intersects.RectangleToRectangle(generator.airFilterCover.getBounds(), generator.targetairFilterCoverON.getBounds())) {
+                generator.airFilterCover.x = generator.targetairFilterCoverON.x
+                generator.airFilterCover.y = generator.targetairFilterCoverON.y
+                generator.airFilterCover.disableInteractive();
+                console.log("Air Filter Cover snapped into place");
+                generator.airFilterStep++;
+                generator.targetairFilterCoverON.setVisible(false);
+            }
+        }
+
+        if(gameObject === generator.airFilterDirty) {
+            if(Phaser.Geom.Intersects.RectangleToRectangle(generator.airFilterDirty.getBounds(), generator.targetairFilterDirty.getBounds())) {
+                generator.airFilterDirty.x = generator.targetairFilterDirty.x
+                generator.airFilterDirty.y = generator.targetairFilterDirty.y - 25
+                generator.airFilterDirty.disableInteractive();
+                console.log("Air Filter Dirty snapped into place");
+                generator.airFilterStep++;
+                generator.targetairFilterDirty.setVisible(false);
+                generator.targetairFilterClean.setVisible(true);
+            }
+        }
+
+        if(gameObject === generator.airFilterClean) {
+            if(Phaser.Geom.Intersects.RectangleToRectangle(generator.airFilterClean.getBounds(), generator.targetairFilterClean.getBounds())) {
+                generator.airFilterClean.x = generator.targetairFilterClean.x
+                generator.airFilterClean.y = generator.targetairFilterClean.y - 25
+                generator.airFilterClean.disableInteractive();
+                console.log("Air Filter Clean snapped into place");
+                generator.airFilterStep++;
+                generator.targetairFilterCoverON.setVisible(true);
+            }
+        }
 
     }
 
     execute(scene, generator) {
+
+        //console.log(generator.airFilterStep);
+
+        if(generator.airFilterStep === 1) {
+
+            
+
+        } else if(generator.airFilterStep === 2) {
+
+            
+
+        } else if(generator.airFilterStep === 3) {
+
+            generator.airFilterCover.setInteractive();
+
+        } else if (generator.airFilterStep === 4) {
+
+            generator.generatorFSM.transition('sparkPlug');
+
+        }
 
     }
 }
